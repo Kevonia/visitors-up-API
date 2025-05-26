dbefce5292cb57b527b9ec9f17eb2b99a1ee7c00
Kevoniat <Kevonia123@gmail.com>
2025-05-26 00:55:58 -0500

    fix route

--- app/routers/visitor.py
+++ app/routers/visitor.py
@@ -41,3 +41,19 @@ def delete_visitor(visitor_id: str, db: Session = Depends(get_db), current_user:
     if db_visitor is None:
         raise HTTPException(status_code=404, detail="Visitor not found")
     return db_visitor
+
+
+# Get all visitors of the selected in users
+@router.get("/visitors/user/{user_email}", response_model=list[schemas.Visitor])
+def read_visitors(user_email,skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: schemas.UserBase = Depends(get_current_user)):
+    user = crud.get_user_by_email(db, email=user_email)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.resident is None:
+        raise HTTPException(status_code=404, detail="Resident not found")
+
+    visitor = crud.get_visitors_by_resident(
+        db, user.resident.id, skip=skip, limit=limit)
+    if visitor is None:
+        raise HTTPException(status_code=404, detail="Visitor not found")
+    return visitor
